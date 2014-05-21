251ea2dbe0aaf6a5a70db8ec1780ef8279594322
RYAN <b9807014@mail.ntust.edu.tw>
2014-05-21 22:57:40 +0800

    New feature: 1. adding generate synthesis data(e.g. synthesis rotation) Undo feature: 1. automatically generate synthesis data with random rotation 2. draw learning rate

--- autotrain.cpp
+++ autotrain.cpp
@@ -65,6 +65,27 @@ bool executeCommandLine(std::vector<std::string> cmdLine) {
 	}
 }
 
+cv::Mat rotate(cv::Mat src, double angle, double scale = 1) {
+	double radius = angle * CV_PI/180;
+	double alpha = cos(radius) * scale;
+	double beta = sin(radius) * scale;
+	int nCols = static_cast<int>(src.cols * fabs(alpha) + src.rows * fabs(beta));
+	int nRows = static_cast<int>(src.cols * fabs(beta) + src.rows * fabs(alpha));
+
+    cv::Point2f center(src.cols/2., src.rows/2.);
+    cv::Mat m = cv::getRotationMatrix2D(center, angle, 1.0);
+	m.ptr<double>(0)[2] +=  static_cast<int>((nCols - src.cols)/2);
+	m.ptr<double>(1)[2] +=	static_cast<int>((nRows - src.rows)/2);
+	
+	cv::Mat dst;
+    warpAffine(src, dst, m, cv::Size(nCols, nRows));
+    return dst;
+}
+
+void synthesisData(std::vector<std::string>posImgs, std::string synDir, int synNum) {
+
+}
+
 int main(int argc, char *argv[])
 {
 	if(argc == 1) {
@@ -73,18 +94,27 @@ int main(int argc, char *argv[])
 			  << "[-pos <postive_collection_directory>]\n"
 			  << "[-neg <negative_collection_directory>]\n"
 			  << "[-vec <output_vec_directory>]\n"
+			  << "[-w <sample_width = 24>]\n[-h <sample_height = 24>]\n"
+			  << "----------synthesis paramters----------\n"
+			  << "[-rot <synthesis_rotation>]\n"
+			  << "[-syn <synthesis_number = 2>]\n"
+			  << "[-synDir <syntehsis_data_collection_directory>\n]"
+			  << "----------optimal paramters----------\n"
 			  << "[-opt <optimize_result>]\n"
-			  << "[-num <num_of_divided_training = 4>]"
-			  << "[-w <sample_width = 24>]\n[-h <sample_height = 24>]\n";
+			  << "[-num <number_of_divided_training = 4>]";
+			  
 	}
 
-	bool opt = "f";	
+	bool opt = false;
+	bool rot = false;
 	int w = 24;
 	int h = 24;
+	int synNum = 2;
 	int num = 4;
 	std::string vecDir;
 	std::string posColDir;
 	std::string negColDir;
+	std::string synDir = "./synData";
 
 	for(int i = 1; i < argc; i++) {
 		if(!strcmp(argv[i], "-pos")) {
@@ -95,6 +125,15 @@ int main(int argc, char *argv[])
 		}
 		if(!strcmp(argv[i], "-vec")) {
 			vecDir = argv[++i];
+		}
+		if(!strcmp(argv[i], "-rot")) {
+			rot = true;
+		}
+		if(!strcmp(argv[i], "-syn")) {
+			synNum = atoi(argv[++i]);
+		}
+		if(!strcmp(argv[i], "synDir")) {
+			synDir = argv[++i];
 		}
 		if(!strcmp(argv[i], "-opt")) {
 			opt = true;
